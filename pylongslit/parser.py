3ec2e41070cd33d56a76db85ede8a3082eb99633
Jiazhe Li <djw465@alumni.ku.dk>
2025-09-08 16:22:33 +0200

    Add the support of configuration folder
    The configuration files can be separated in different json files under a directory.

--- pylongslit/parser.py
+++ pylongslit/parser.py
@@ -7,28 +7,67 @@ The path to the config file is set in the __init__.py file.
 import json
 from pylongslit.logger import logger
 import os
+from pathlib import Path
 from pylongslit import CONFIG_FILE_PATH
 
+def read_config_dir(config_path):
+    """
+    Reads the config files from the specified directory.
+
+    Parameters
+    ----------
+    config_path : Path
+        Path to the config files directory.
+
+    Returns
+    -------
+    dict
+        Dictionary containing the user parameters.
+    """
+    merged_data = {}
+    for config_file in config_path.glob("*.json"):
+        logger.info(f"Found config file: {config_file.name}")
+        with open(config_file, "r") as file:
+            data = json.load(file)
+
+            for key, value in data.items():
+                if key in merged_data:
+                    logger.error(f"Key '{key}' found in multiple config files. Please ensure all keys are unique.")
+                    raise KeyError(f"Duplicate key '{key}' in config files.")
+
+            merged_data.update(data)
+
+            logger.info(f"Loaded config file: {config_file.name}")
+    return merged_data
 
 # Open the config file
 try:
-    file = open(CONFIG_FILE_PATH, "r")
-except FileNotFoundError:
+    config_file_path = Path(CONFIG_FILE_PATH)
+    if not config_file_path.exists():
+        logger.error("Config file not found.")
+        logger.error(
+            "Make sure a config file exists. \n"
+            "See the docs at:\n"
+            "https://kostasvaleckas.github.io/PyLongslit/"
+        )
+        raise FileNotFoundError(f"Config file {CONFIG_FILE_PATH} does not exist")
+    elif config_file_path.is_file():
+        file = open(CONFIG_FILE_PATH, "r")
+        logger.info("Config file found. Loading user parameters...")
+        data = json.load(file)
+        file.close()
+    elif config_file_path.is_dir():
+        data = read_config_dir(config_file_path)
+except Exception as e:
 
-    logger.error("Config file not found.")
+    logger.error(f"Config file Error.{e}")
     logger.error(
-        "Make sure a config file exists. \n"
+        "Make sure a config file exists and everything is correct. \n"
         "See the docs at:\n"
         "https://kostasvaleckas.github.io/PyLongslit/"
     )
 
     exit()
-
-logger.info("Config file found. Loading user parameters...")
-
-data = json.load(file)
-
-file.close()
 
 # Define parameter groups for easier access
 try:
